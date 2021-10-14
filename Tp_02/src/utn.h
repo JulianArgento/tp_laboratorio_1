fde58ebfc9c8cff64917d2e5c73bfded47df37ec
JulianArgento <81318510+JulianArgento@users.noreply.github.com>
2021-10-14 20:29:18 -0300

    Delete Tp_02 directory

--- Tp_02/src/utn.h
+++
@@ -1,9 +0,0 @@
-#ifndef UTN_H_INCLUDED
-#define UTN_H_INCLUDED
-
-int esNumerico(char str[]);
-int esSoloLetras(char str[]);
-int getStringLetras(char mensaje[],char input[]);
-void getString(char mensaje[],char input[]);
-
-#endif // UTN_H_INCLUDED
